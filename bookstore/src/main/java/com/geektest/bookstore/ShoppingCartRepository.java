f8198bc73a865d0e364ad8d0d4c4adb3abadfcbe
Roseline <rdami007@fiu.edu>
2025-02-28 21:06:26 -0500

    Get requests for subtotal and cart items

--- bookstore/src/main/java/com/geektest/bookstore/ShoppingCartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.geektest.bookstore;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShoppingCartRepository extends CrudRepository<ShoppingCart, Long> {
-    //repository that takes the shopping cart object and the userID
-    
-}
