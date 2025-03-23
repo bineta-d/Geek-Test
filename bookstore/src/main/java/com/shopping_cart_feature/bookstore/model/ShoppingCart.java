4aa0330418d829a084b6b8483786044f804bd4b0
Roseline <rdami007@fiu.edu>
2025-03-23 11:18:55 -0400

    added post request

--- bookstore/src/main/java/com/shopping_cart_feature/bookstore/model/ShoppingCart.java
+++ bookstore/src/main/java/com/shopping_cart_feature/bookstore/model/ShoppingCart.java
@@ -9,14 +9,24 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Data
 public class ShoppingCart {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long cartId; //unique primary key
     
+    @Column(name = "user_id")
     private long userId;
 
     private long book; //a book by its ID
+
+    public ShoppingCart() {}
+
+    public ShoppingCart(long userId, long bookId) {
+        this.userId = userId;
+        this.book = bookId;
+    }
 
     public long getBook() {
         return this.book;
