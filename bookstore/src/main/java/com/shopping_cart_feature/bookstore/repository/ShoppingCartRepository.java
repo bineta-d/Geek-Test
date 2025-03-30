af90d8f7fa2a277e61f1dda695ead7626bacc8bc
Roseline <rdami007@fiu.edu>
2025-03-30 13:03:17 -0400

    delete mapping shopping cart

--- bookstore/src/main/java/com/shopping_cart_feature/bookstore/repository/ShoppingCartRepository.java
+++ bookstore/src/main/java/com/shopping_cart_feature/bookstore/repository/ShoppingCartRepository.java
@@ -2,6 +2,7 @@ package com.shopping_cart_feature.bookstore.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
@@ -12,4 +13,8 @@ import com.shopping_cart_feature.bookstore.model.ShoppingCart;
 public interface ShoppingCartRepository extends ListCrudRepository<ShoppingCart, Long> {
     //repository that takes the shopping cart object and the userID
     List<ShoppingCart> findByUserId(Long userId);
+
+    @Modifying
+    @Query(value = "DELETE FROM shopping_cart WHERE user_id = :userId AND book = :book", nativeQuery = true)
+    void deleteCartItem(@Param("userId") Long userId, @Param("book") Long book) throws IllegalArgumentException;
 }
