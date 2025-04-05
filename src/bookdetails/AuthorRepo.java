1947295270e439f20f9683a005b3518b9396aa09
Victoria <vcout003@fiu.edu>
2025-04-05 15:48:46 -0600

    removing src folder since src2 now has those files

--- src/bookdetails/AuthorRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.book_details.repo;
-
-import com.example.book_details.models.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepo extends JpaRepository<Author, Integer> {
-}
